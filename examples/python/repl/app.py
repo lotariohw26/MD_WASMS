a75f4bc67f577d1f9dd0263d907dd3caebc925b3
lotariohw26 <lotariohw26@protonmail.com>
2024-10-07 00:05:13 +0200

    tryagain

--- examples/python/repl/app.py
+++ examples/python/repl/app.py
@@ -1,7 +1,4 @@
 import sympy, pandas, numpy, math, polysolver
-import pandas as pd
-import polysolver as plsv
-import math                                                                   
 from sympy import solve, Eq, symbols, latex, simplify, diff, poly, sympify, Matrix, pprint, collect, expand, Poly, Symbol, Pow
 def rall(sel=[0, 0, 0]):
     n, m = symbols('n m')
@@ -233,13 +230,11 @@ c1 = pareq(ste=str(test[1][6]),m1=m1v,m2=m2v,m3=m3v,n1=n1v,n2=n2v,n3=n3v)
 c2 = pareq(ste=str(test[1][7]),m1=m1v,m2=m2v,m3=m3v,n1=n1v,n2=n2v,n3=n3v)
 c3 = pareq(ste=str(test[1][8]),m1=m1v,m2=m2v,m3=m3v,n1=n1v,n2=n2v,n3=n3v)
 
-
-
 results = []
 for nr in range(20):  # Example: looping from 0 to 19, you can change the range as needed
     lhs = test[2]['d'][nr]
     rhs = test[2]['expr'][nr]
-    pareq_result = plsv.pareq(
+    pareq_result = pareq(
         str(rhs),
         a1=a1, a2=a2, a3=a3,
         b1=b1, b2=b2, b3=b3,
@@ -253,11 +248,19 @@ for nr in range(20):  # Example: looping from 0 to 19, you can change the range 
         'pareq_result': pareq_result
     })
 
-df_results = pd.DataFrame(results)
+df_results = pandas.DataFrame(results)
 
 # Output
 a1, a2, a3, b1, b2, b3, c1, c2, c3
 df_results
+
+
+
+
+
+
+
+
 
 
 
